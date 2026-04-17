cd92895cd8ff9e92762b4ef389aef2cc6f10eb2c
rajnish2323 <rajnishk126@gmail.com>
2026-04-17 09:22:08 +0530

    basic 1

---
+++ raj.py
@@ -0,0 +1,15 @@
+def step1(data):
+    return data + 2
+
+def step2(data):
+    return data * 3
+
+def step3(data):
+    return data - 1
+
+# pipeline execution
+data = 5
+
+result = step3(step2(step1(data)))
+
+print(result)
